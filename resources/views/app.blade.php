bb5aec707cb3ffb80437d9fad8ad717061fb1046
Adam Bailey <adamjbailey7@gmail.com>
2021-12-17 10:13:17 -0800

    add new font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Gwendolyn:wght@700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
