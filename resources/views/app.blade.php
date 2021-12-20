d300cf5bed6cb225c1ba00fd2ca717b189bebe3c
Adam Bailey <adamjbailey7@gmail.com>
2021-12-20 06:40:38 -0800

    fix google tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,13 @@
 
         @env ('production')
             <!-- Global site tag (gtag.js) - Google Analytics -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id="{{ config('services.google.site_tag') }}"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.site_tag') }}"></script>
                 <script>
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    gtag('config', {{ config('services.google.site_tag') }});
+                    gtag('config', '{{ config('services.google.site_tag') }}');
                 </script>
         @endenv
 
