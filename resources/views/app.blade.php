cc51b54841c8b187494f299e884e31a14d9210f6
Adam Bailey <adamjbailey7@gmail.com>
2021-12-19 21:50:10 -0800

    add google tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'FamilyTribute') }}</title>
+
+        @env ('production')
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id="{{ config('services.google.site_tag') }}></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', {{ config('services.google.site_tag') }});
+                </script>
+        @endenv
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
