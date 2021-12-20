7614ce2e98868ccd04feb1ecf55d5154ce4d04e6
Adam Bailey <12718450+isAdamBailey@users.noreply.github.com>
2021-12-19 22:20:43 -0800

    add missing quote to gta scrip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
         @env ('production')
             <!-- Global site tag (gtag.js) - Google Analytics -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id="{{ config('services.google.site_tag') }}></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id="{{ config('services.google.site_tag') }}"></script>
                 <script>
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
