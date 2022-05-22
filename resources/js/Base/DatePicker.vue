2d85d5be0570cc522ed80b449f45ee861bbd2913
Adam Bailey <adamjbailey7@gmail.com>
2022-05-22 07:04:59 -0700

    upgrade breaking datetime picker dependency

--- resources/js/Base/DatePicker.vue
+++ resources/js/Base/DatePicker.vue
@@ -11,8 +11,8 @@
 
 <script>
 import { defineComponent } from "vue";
-import Datepicker from "vue3-date-time-picker";
-import "vue3-date-time-picker/dist/main.css";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 export default defineComponent({
     components: { Datepicker },
